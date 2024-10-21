b9a2cecffca599c2894e2e1c558ce2ed7742cc32
Muhammaddavron <muhammaddavron123@gmail.com>
2024-10-21 23:13:25 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -9,6 +9,7 @@ function Hisobla(nomi, narxi) {
     let zakaz =confirm("Siz buyurtma bermoqchimisiz?")
     if (zakaz) {
         alert("Zakazingiz qabul qilindi")
+        alert("!45 minut ichida yetkazib berilmasa zakaz tekin!")
     }
     else{
         alert("!Zakazingiz qabul qilinmadi!")
